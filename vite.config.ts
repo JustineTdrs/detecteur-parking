d4e0ef644c8d4ab2adaca199bb4ff2dde72e7f80
JustineTadros <71759215+JustineTadros@users.noreply.github.com>
2025-02-06 00:17:36 +0200

    🚀 Déploiement sur GitHub Pages

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/detecteur-parking/", // 🔥 Assure-toi que c'est bien le bon nom de repo GitHub
-});
